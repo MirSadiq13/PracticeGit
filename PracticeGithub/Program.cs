22b2f7524d1be6f5e4f8531b85692b26f551036c
Mirsadiq <Mirsadiq@USER>
2025-10-02 17:16:32 +0400

    adasafa

--- PracticeGithub/Program.cs
+++ PracticeGithub/Program.cs
@@ -1,25 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Test Feature Branch");
-
-Console.WriteLine("Checkout Feature Branch");
-
-Console.WriteLine();
-
-Console.WriteLine("MErge Feature Branch");
-
-
-
-
-
-
-
-
-
-
-
-
-Console.WriteLine("MErge Feature Branch 2");
